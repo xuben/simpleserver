37b28983b4b5c5e550eade8060bbf6ba4f03f755
xuben <shaci1234@aliyun.com>
2015-08-18 00:27:37 +0800

    Update SimpleServer.java
    flash策略文件处理代码重构

--- src/ben/gcld/simpleserver/SimpleServer.java
+++ src/ben/gcld/simpleserver/SimpleServer.java
@@ -28,12 +28,9 @@ public class SimpleServer implements Runnable {
 	private LinkedBlockingQueue<Object> sendQueue;
 	// 从代理服务器接收到的数据包队列
 	private LinkedBlockingQueue<Object> receiveQueue;
-	// flash策略文件处理
-	private FlashPolicyHandler flashPolicyHandler;
 	
 	public SimpleServer(Socket socket) {
 		this.socket = socket;
-		this.flashPolicyHandler = new FlashPolicyHandler();
 	}
 
 	@Override
@@ -51,19 +48,13 @@ public class SimpleServer implements Runnable {
 					return;
 				}
 				// 处理请求
-				result = flashPolicyHandler.handle(socket);
+				result = checkFlashPolicy();
 				if (result == 0) { // flash策略文件请求
 					System.out.println("[SimpleServer] policy file request");
 					return;
 				} else if (result > 0) { // 不是flash策略文件请求
-					// 代理模式
-					if (!ServerConfig.STANDALONE_MODE) {
-						sendQueue = new LinkedBlockingQueue<Object>();
-						receiveQueue = new LinkedBlockingQueue<Object>();
-						// 启动代理线程
-						proxy = new Thread(new SimpleProxy(sendQueue, receiveQueue));
-						proxy.start();
-					}
+					// 初始化代理
+					initProxy();
 					readLength = result;
 					break;
 				} else {
@@ -177,6 +168,47 @@ public class SimpleServer implements Runnable {
 				proxy.interrupt();
 			}
 			System.out.println("[SimpleServer] disconnected");
+		}
+	}
+	
+	/**
+	 * 处理flash策略文件请求
+	 * @return 未收到请求返回-1 处理策略文件返回0 否则返回包长度
+	 * @throws IOException
+	 */
+	private int checkFlashPolicy() throws IOException {
+		InputStream in = socket.getInputStream();
+		if (in.available() >= 4) {
+			byte[] content = new byte[4];
+			in.read(content);
+			// 判断是否为策略文件请求
+			if (new String(content).equalsIgnoreCase("<pol")) {
+				// 读取所有内容
+				while (in.available() > 0) {
+					in.read();
+				}
+				// 返回响应
+				OutputStream out = socket.getOutputStream();
+				out.write(ServerConfig.FLASH_POLICY_RESPONSE);
+				return 0;
+			}
+			// 不是策略文件请求
+			return IOUtil.readInt(content);
+		}
+		return -1;
+	}
+	
+	/**
+	 * 初始化代理
+	 */
+	private void initProxy() {
+		// 代理模式
+		if (!ServerConfig.STANDALONE_MODE) {
+			sendQueue = new LinkedBlockingQueue<Object>();
+			receiveQueue = new LinkedBlockingQueue<Object>();
+			// 启动代理线程
+			proxy = new Thread(new SimpleProxy(sendQueue, receiveQueue));
+			proxy.start();
 		}
 	}
 }
